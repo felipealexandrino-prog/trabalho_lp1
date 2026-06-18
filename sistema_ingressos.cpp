645368df94e54254dfefd7455beee30fd482ba17
Felipe Alexandrino <falexandrino388@gmail.com>
2026-06-17 21:44:26 -0300

    Atualização relatório felipe por vendas partida

--- sistema_ingressos.cpp
+++ sistema_ingressos.cpp
@@ -47,22 +47,72 @@ struct ItemVenda {
     int idIngresso;
 };
 
+struct Relatorio {
+    // 1 - Total de vendas por Funcionário
+    // 2 - Total de vendas por Cliente
+    // 3 - Total de vendas por Partida
+    // 4 - Partida que mais vendeu ingressos
+};
+
 // ─────────────────────────────────────────
-//  VETORES DE CONTROLE (dinâmicos)
+//  VETORES DE CONTROLE
 // ─────────────────────────────────────────
 
 vector<Funcionario> funcionarios;
-vector<Cliente>     clientes;
-vector<Partida>     partidas;
-vector<Ingresso>    ingressos;
-vector<Venda>       vendas;
-vector<ItemVenda>   itensVenda;
+vector<Cliente> clientes;
+vector<Partida> partidas;
+vector<Ingresso> ingressos;
+vector<Venda> vendas;
+vector<ItemVenda> itensVenda;
 
 // ─────────────────────────────────────────
-//  MAIN (placeholder)
+//  RELATÓRIO DE VENDAS POR PARTIDA : Felipe
+// ─────────────────────────────────────────
+
+void relatorioPartida(int totalPartidas, int totalVendas) {
+
+    if (totalVendas == 0) {
+        cout << "\nNenhuma venda cadastrada.\n";
+        return;
+    }
+
+    cout << "\n===== RELATORIO DE VENDAS POR PARTIDA =====\n";
+
+    for (int i = 0; i < totalPartidas; i++) {
+        int vendasPartida = 0;
+
+        for (int j = 0; j < totalVendas; j++) {
+            if (vendas[j].idPartida == partidas[i].id) {
+                vendasPartida++;
+            }
+        }
+
+        cout << "\nPartida: "
+             << partidas[i].timeCasa
+             << " x "
+             << partidas[i].timeVisitante;
+
+        cout << "\nData: "
+             << partidas[i].data;
+
+        cout << "\nTotal de vendas: "
+             << vendasPartida
+             << "\n";
+    }
+}
+
+// ─────────────────────────────────────────
+//  MAIN
 // ─────────────────────────────────────────
 
 int main() {
     cout << "Sistema de Gestão de Ingressos e Sócio Torcedor" << endl;
+
+    int totalPartidas = partidas.size();
+    int totalVendas = vendas.size();
+
+    // relatorioPartida(totalPartidas, totalVendas); -> Utiliza essa função quando o switch for opçãao 3!
+    //maria rita safada traira nunca ira voltar
+
     return 0;
 }
